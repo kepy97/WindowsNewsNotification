ddb77c010c1733fad2e7bd0c353c514154627d2c
Keyul Patel <keyulvpatel97@gmail.com>
2019-12-31 12:22:17 -0600

    Cred update

--- mail.py
+++ mail.py
@@ -3,8 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def sendmail(result,tomail,header):
-    #fromaddr = "hellohi8878@gmail.com"
-    fromaddr = "keyul.kepy@gmail.com"
+    fromaddr = "XXXXXXX@gmail.com"
     toaddr = tomail
     msg = MIMEMultipart()
     msg['From'] = fromaddr
@@ -16,7 +15,7 @@ def sendmail(result,tomail,header):
  
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(fromaddr, "hellohi8878")
+    server.login(fromaddr, "XXXXX")
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     print('Mail Sent')
